37781639d53da83c1e3dc3237a8c4edd58589302
Joel-109 <joelalac@unisabana.edu.co>
2025-11-22 17:40:07 -0500

    GREEN TDD Transacciones

--- NovaBack/transacciones/models.py
+++ NovaBack/transacciones/models.py
@@ -1,8 +1,19 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator
+from usuarios.models import Usuario
 # Create your models here.
 class Transaccion(models.Model):
     
-    pass
+    def save(self, *args, **kwargs):
+        self.full_clean()  # ← activa validadores
+        super().save(*args, **kwargs)
+
+    usuario = models.ForeignKey(Usuario,on_delete=models.CASCADE,
+        related_name='transacciones')
+    concepto = models.CharField(max_length=200)
+    monto = models.DecimalField(max_digits=12, decimal_places=2,validators=[MinValueValidator(0)])
+    fecha = models.DateTimeField(auto_now_add=True)
+    notas = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     
--- NovaBack/transacciones/tests.py
+++ NovaBack/transacciones/tests.py
@@ -6,10 +6,25 @@ from usuarios.models import Usuario
 
 class TransaccionTestCase(TestCase):
 
-    def crear_transaccion(self):
+    def test_crear_transaccion(self):
+        datos_usuario = {
+            "username": "juan123",
+            "password": "password_seguro",
+            "email": "juan@example.com",
+            "telefono": "555123456",
+            "presupuesto_mensual": 1500.50,
+        }
 
-        Transaccion(
-            usuario = None,
+        user = Usuario.objects.create(
+            username = datos_usuario['username'],
+            password = datos_usuario['password'],
+            email = datos_usuario['email'],
+            telefono = datos_usuario['telefono'],
+            presupuesto_mensual = datos_usuario['presupuesto_mensual']
+        )
+
+        Transaccion.objects.create(
+            usuario = user,
             concepto="Pago Universidad",
             monto = 1000,
             notas = "Pago de la matrícula, del semestre 7"
@@ -19,34 +34,56 @@ class TransaccionTestCase(TestCase):
 
 
 
-    def verificar_usuario_transaccion(self):
-        Transaccion(
-            usuario = None,
+    def test_verificar_nombre_usuario_transaccion(self):
+        datos_usuario = {
+            "username": "pablomillar",
+            "password": "jamaica904",
+            "email": "pablomi2@example.com",
+            "telefono": "425396521",
+            "presupuesto_mensual": 2030.10,
+        }
+
+        user = Usuario.objects.create(
+            username = datos_usuario['username'],
+            password = datos_usuario['password'],
+            email = datos_usuario['email'],
+            telefono = datos_usuario['telefono'],
+            presupuesto_mensual = datos_usuario['presupuesto_mensual']
+        )
+
+        Transaccion.objects.create(
+            usuario = user,
             concepto="Pago Universidad",
             monto = 1000,
             notas = "Pago de la matrícula, del semestre 7"
         )
 
-        self.assertEqual(Transaccion.objects.all()[0].usuario.nombre,usuario.nombre)
+        self.assertEqual(Transaccion.objects.all()[0].usuario.username ,user.username)
 
     
-    def verificar_error_valor_negativo(self):
-        Transaccion(
-            usuario = None,
-            concepto="Pago Universidad",
-            monto = 1000,
-            notas = "Pago de la matrícula, del semestre 7"
+    def test_verificar_error_valor_negativo(self):
+        datos_usuario = {
+            "username": "pablomillar",
+            "password": "jamaica904",
+            "email": "pablomi2@example.com",
+            "telefono": "425396521",
+            "presupuesto_mensual": 2030.10,
+        }
+
+        user = Usuario.objects.create(
+            username = datos_usuario['username'],
+            password = datos_usuario['password'],
+            email = datos_usuario['email'],
+            telefono = datos_usuario['telefono'],
+            presupuesto_mensual = datos_usuario['presupuesto_mensual']
         )
 
         with self.assertRaises(ValidationError):
             Transaccion.objects.create(
-                usuario=None,
-                nombre=None,
-                meta_monto=None,  
-                ahorro_actual=None,
-                tiempo_estimado=None,
-                fecha_objetivo=None,
-                imagen=None,
+                usuario = user ,
+                concepto="Pago Universidad",
+                monto = -1000,
+                notas = "Pago de la matrícula, del semestre 7"
             )
 
 
