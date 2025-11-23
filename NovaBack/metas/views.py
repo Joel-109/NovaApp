43b218bcac220c9e9dcbd37c2e3eff8fee96dc7c
Camilo-Ota <camilootri@unisabana.edu.co>
2025-11-22 19:44:29 -0500

    URLS

--- NovaBack/metas/views.py
+++ NovaBack/metas/views.py
@@ -1,6 +1,4 @@
 from rest_framework import viewsets, permissions
-from rest_framework.decorators import action
-from rest_framework.response import Response
 from .models import Meta, AporteMeta
 from .serializers import MetaSerializer, MetaCreateSerializer, AporteMetaSerializer, AporteMetaCreateSerializer
 
--- NovaBack/usuarios/views.py
+++ NovaBack/usuarios/views.py
@@ -1,39 +1,17 @@
-from rest_framework.views import APIView
-from rest_framework import  permissions, status
-from rest_framework.decorators import action
-from rest_framework.response import Response
-from django.contrib.auth import authenticate
+from rest_framework import viewsets, permissions
 from usuarios.models import Usuario
-from .serializers import UsuarioSerializer,UsuarioCreateSerializer
+from .serializers import UsuarioSerializer, UsuarioCreateSerializer
 
-'''
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
-    permission_classes = [permissions.AllowAny]
-
-'''
-
-# Create your views here.
-class UsuarioAPIView(APIView):
-
-    def post(self, request):
-        serializer = UsuarioSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
-
-class UsuarioCreateAPIView(APIView):
-    permission_classes = [permissions.AllowAny]
-
-    def get(self, request):
-        usuarios = Usuario.objects.all()   
-        serializer = UsuarioCreateSerializer(usuarios, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = UsuarioCreateSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return UsuarioCreateSerializer
+        return UsuarioSerializer
+    
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Usuario.objects.all()
+        return Usuario.objects.filter(id=self.request.user.id)
